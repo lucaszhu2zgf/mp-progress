462e0e95bfa9a294a67d92e0a3dc6ef98ca05113
lucaszhu <lucaszhu@webank.com>
2020-04-26 14:20:34 +0800

    FIX: 根据参数调整逻辑

--- README.md
+++ README.md
@@ -10,18 +10,37 @@ $ npm install mp-progress --save
 ```
 import MpProgress from 'mp-progress';
 ...
-
-MpProgress({
+// 初始化
+const mprogress = new MpProgress({
   canvasId: 'progress',
-  percentage: 60,
+  canvasSize: {width: 400, height: 400},
   backgroundBar: {width: 12, fillStyle: '#f0f0f0'},
-  foregroundBar: width: 12, fillStyle: [{position: 0, color: '#56B37F'}, {position: 1, color: '#c0e674'}]
+  foregroundBar: {width: 12, fillStyle: [{position: 0, color: '#56B37F'}, {position: 1, color: '#c0e674'}]}
 });
+
+// 开始绘制进度，60代表60%
+mprogress.draw(60);
 
 ...
 <canvas class="canvas" canvas-id="progress"></canvas>
 ```
-## 参数列表
-|参数名|解析|必要参数|
-|:----|:-----|:---:|
-|canvasId|页面canvas的id|是|
+## 参数说明
+|参数名|数据类型|解析|必要参数|
+|:----|:-----|:-----|:---:|
+|canvasId|String|页面canvas的id|是|
+|canvasSize|Object|canvas画布的大小，对应单位是rpx，只需要传入数字即可，比如canvas的宽高分别是200rpx、300rpx，对应传入参数为{width: 200, height: 300}|是|
+|backgroundBar|Object|第一层圆弧额样式配置，一般作为背景铺满完整圆弧|是|
+|foregroundBar|Object|第二层圆弧额样式配置，一般作为动态进度根据percent参数决定铺满多少圆弧，如果需要使用渐变颜色，fillStyle需要按照数组的格式传入各个点的位置和颜色信息|否|
+|needDot|Boolean|是否需要显示进度点|否|
+|dotStyle|Array|进度点样式配置，最多支持两种样式叠加，为保证进度点的清晰显示第一个点默认添加阴影。如果第一个进度点直径比进度条更大时，为了保证在画布内完整显示进度点，绘制的进度条圆圈必须减少对应的差值|否|
+
+## 绘制函数draw
+考虑到进度条在一个页面可能多次更新，所以当组件实例化完成之后，需要手动调用drwa方法去画图
+|参数名|数据类型|解析|必要参数|
+|:----|:-----|:-----|:---:|
+|percentage|Number|圆弧百分比，比如传入的是60，只会逆时针渲染剩余的60%圆弧|是|
+
+## 其他说明
+|问题描述|解决方案|
+|:----|:-----|
+|如何设置弧线端点样式|可以在backgroundBar或者foregroundBar中增加lineCap参数，例如：`foregroundBar: {width: 12, lineCap: 'round', fillStyle: '#56B37F'}`有效值可以[查看文档](https://developers.weixin.qq.com/miniprogram/dev/api/canvas/CanvasContext.setLineCap.html)|
--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@
  */
 class MpProgress{
   constructor(options){
-    const {canvasId, canvasSize = {width: 0, height: 0}, percentage = 0, backgroundBar = {}, foregroundBar = {}, needDot = false, dotStyle = []} = options;
+    const {canvasId, canvasSize = {width: 0, height: 0}, backgroundBar, foregroundBar, needDot = false, dotStyle = []} = options;
     if (canvasId) {
       this._context = wx.createContext();
       this._options = {
+        canvasId,
         needDot,
         dotStyle,
         canvasSize,
-        percentage,
         backgroundBar,
         foregroundBar
       };
@@ -23,63 +23,102 @@ class MpProgress{
       throw '[初始化失败]: 缺少canvasId';
     }
   }
-  drawFn(){
-    // 需要旋转的角度
-    const deg = ((this._options.percentage).toFixed(2))*2*Math.PI;
-    // 圆圈半径
-    const r = this.convertLength(Math.round(this._options.canvasSize.width/2 - backgroundBar.width/2));
+  draw(percentage){
+    if (typeof percentage === 'undefined') {
+      console.warn('[绘图过程出现错误]: 调用draw方法必须传入百分比参数');
+      return;
+    }
+    this._options.percentage = +percentage || 0;
+    try {
+      // 需要旋转的角度
+      const deg = ((this._options.percentage/100).toFixed(2))*2*Math.PI;
+      // 圆圈半径
+      let _r = Math.round(this._options.canvasSize.width/2 - this._options.backgroundBar.width) - 6;
 
-    // 更换原点
-    const originX = Math.round(this._options.canvasSize.width/2);
-    const originY = Math.round(this._options.canvasSize.height/2);
-    context.translate(convertLength(originX), convertLength(originY));
-    // arc原点默认为3点钟方向，需要调整到12点
-    context.rotate(-90 * Math.PI / 180);
+      if (this._options.needDot) {
+        if (this._options.dotStyle.length > 0) {
+          const circleR = this._options.dotStyle[0].r;
+          const barWidth = this._options.backgroundBar.width
+          if (circleR > barWidth) {
+            // 4-阴影大小
+            _r -= circleR - barWidth + 4;
+          }
+        }else{
+          console.warn('参数dotStyle不完整，请检查');
+          return;
+        }
+      }
+      this._r = this.convertLength(_r);
 
-    // 画底圈
-    context.beginPath();
-    context.arc(0, 0, r, 0, 2*Math.PI);
-    context.setLineWidth(this._options.backgroundBar.width);
-    context.setStrokeStyle(this._options.backgroundBar.fillStyle);
-    context.stroke();
+      // 更换原点
+      const originX = Math.round(this._options.canvasSize.width/2);
+      const originY = Math.round(this._options.canvasSize.height/2);
+      this._context.translate(this.convertLength(originX), this.convertLength(originY));
+      // arc原点默认为3点钟方向，需要调整到12点
+      this._context.rotate(-90 * Math.PI / 180);
 
-    // 画进度圈
-    let foregroundBarFillStyle = '';
-    const {fillStyle} = this._options.foregroundBar;
-    if (typeof this._options.foregroundBar.fillStyle === 'string') {
+      const isForegroundBarConfig = typeof this._options.foregroundBar !== 'undefined' ? true : false;
+
+      // 画第一层
+      this._context.beginPath();
+      this._context.arc(0, 0, this._r, 0, isForegroundBarConfig ? 2*Math.PI : deg);
+      this._context.setLineWidth(this._options.backgroundBar.width);
+      this._context.setStrokeStyle(this.generateBarFillStyle(this._options.backgroundBar.fillStyle));
+      const backgroundBarLineCap = this._options.backgroundBar.lineCap;
+      if (backgroundBarLineCap) {
+        this._context.setLineCap(backgroundBarLineCap);
+      }
+      this._context.stroke();
+
+      // 画第二层
+      if (isForegroundBarConfig) {
+        this._context.beginPath();
+        this._context.arc(0, 0, this._r, 0, deg);
+        this._context.setLineWidth(this._options.foregroundBar.width);
+        const foregroundBarLineCap = this._options.foregroundBar.lineCap;
+        if (foregroundBarLineCap) {
+          this._context.setLineCap(foregroundBarLineCap);
+        }
+        // this._context.setLineCap('round');
+        this._context.setStrokeStyle(this.generateBarFillStyle(this._options.foregroundBar.fillStyle));
+        this._context.stroke();
+      }
+
+      if (this._options.needDot) {
+        this.drawBarCoordinateDot();
+      }
+
+      wx.drawCanvas({
+        canvasId: this._options.canvasId,
+        actions: this._context.getActions()
+      });
+    } catch (err) {
+      console.warn('[绘图过程出现错误]: ', err);
+    }
+  }
+  /**
+   * 计算填充颜色
+   * @param {config} 传入的颜色配置
+   */
+  generateBarFillStyle(config){
+    if (typeof config === 'string') {
       // 单一色彩
-      foregroundBarFillStyle = fillStyle;
+      return config;
     }else{
       // 渐变色彩
-      const grd = context.createLinearGradient(0, 0, 100, 90);
-      for (let i = 0; i < fillStyle.length; i++) {
-        const item = fillStyle[i];
+      const grd = this._context.createLinearGradient(0, 0, 100, 90);
+      for (let i = 0; i < config.length; i++) {
+        const item = config[i];
         grd.addColorStop(item.position, item.color);
       }
-      foregroundBarFillStyle = grd;
+      return grd;
     }
-
-    context.beginPath();
-    context.arc(0, 0, r, 0, deg);
-    context.setLineWidth(this._options.foregroundBar.width);
-    // context.setLineCap('round');
-    context.setStrokeStyle(foregroundBarFillStyle);
-    context.stroke();
-
-    if (needDot) {
-      this.drawBarCoordinateDot();
-    }
-
-    wx.drawCanvas({
-      canvasId: this._options.canvasId,
-      actions: context.getActions()
-    });
   }
   /**
    * convertLength
    * 小程序长度单位转换函数
    */
-  static convertLength(length) {
+  convertLength(length) {
     return +Math.round(wx.getSystemInfoSync().windowWidth * length / 750);
   }
   /**
@@ -87,19 +126,23 @@ class MpProgress{
    * @param {context} Object canvas 2d context
    * @param {style} Object 圆的样式参数
    */
-  static drawCircleWithFillStyle(style){
+  drawCircleWithFillStyle(style){
+    console.log(style)
     this._context.beginPath();
     this._context.arc(style.x, style.y, this.convertLength(style.r), 0, 2 * Math.PI);
     this._context.setFillStyle(style.fillStyle || '#ffffff');
+    if (style.needShadow) {
+      this._context.setShadow(0, 0, this.convertLength(style.r/2), 'rgba(86,179,127,0.5)');
+    }
     this._context.fill();
   }
   /**
    * drawBarCoordinateDot
    * @param {percentage} Number 旋转百分比
    */
-  static drawBarCoordinateDot(){
+  drawBarCoordinateDot(){
     // 数学夹脚
-    const mathDeg = ((this._options.percentage).toFixed(2))*360;
+    const mathDeg = ((this._options.percentage/100).toFixed(2))*360;
     // 计算弧度
     let radian = '';
     // 三角函数cos=y/r，sin=x/r，分别得到小点的x、y坐标
@@ -108,79 +151,38 @@ class MpProgress{
     if (mathDeg <= 90) {
       // 求弧度
       radian = 2*Math.PI/360*mathDeg;
-      x = Math.round(Math.cos(radian)*r);
-      y = Math.round(Math.sin(radian)*r);
+      x = Math.round(Math.cos(radian)*this._r);
+      y = Math.round(Math.sin(radian)*this._r);
     } else if (mathDeg > 90 && mathDeg <= 180) {
       // 求弧度
       radian = 2*Math.PI/360*(180 - mathDeg);
-      x = -Math.round(Math.cos(radian)*r);
-      y = Math.round(Math.sin(radian)*r);
+      x = -Math.round(Math.cos(radian)*this._r);
+      y = Math.round(Math.sin(radian)*this._r);
     } else if (mathDeg > 180 && mathDeg <= 270) {
       // 求弧度
       radian = 2*Math.PI/360*(mathDeg - 180);
-      x = -Math.round(Math.cos(radian)*r);
-      y = -Math.round(Math.sin(radian)*r);
+      x = -Math.round(Math.cos(radian)*this._r);
+      y = -Math.round(Math.sin(radian)*this._r);
     } else{
       // 求弧度
       radian = 2*Math.PI/360*(360 - mathDeg);
-      x = Math.round(Math.cos(radian)*r);
-      y = -Math.round(Math.sin(radian)*r);
+      x = Math.round(Math.cos(radian)*this._r);
+      y = -Math.round(Math.sin(radian)*this._r);
     }
-    console.log(x, y);
+    // console.log(x, y);
 
     if (this._options.dotStyle.length > 0) {
       // 画背景大点
-      this.drawCircleWithFillStyle({x, y, ...dotStyle[0]});
+      this.drawCircleWithFillStyle({x, y, needShadow: true, ...this._options.dotStyle[0]});
+    }else{
+      console.warn('参数dotStyle不完整，请检查');
     }
 
     if (this._options.dotStyle.length > 1) {
-      // 画前景小店
-      this.drawCircleWithFillStyle({x, y, ...dotStyle[1]});
+      // 画前景小点
+      this.drawCircleWithFillStyle({x, y, ...this._options.dotStyle[1]});
     }
   }
 }
 
-
-export default function({canvasId, percentage = 0, needDot = false, dotStyle = []}){
-  if (canvasId) {
-    // 需要旋转的角度
-    const deg = ((percentage).toFixed(2))*2*Math.PI;
-    // 圆圈半径
-    const r = convertLength(170);
-
-    var context = wx.createContext();
-    const grd = context.createLinearGradient(0, 0, 100, 90);
-    grd.addColorStop(0, '#56B37F');
-    grd.addColorStop(1, '#c0e674');
-    // 更换原点
-    context.translate(convertLength(200), convertLength(200));
-    // arc原点默认为3点钟方向，需要调整到12点
-    context.rotate(-90 * Math.PI / 180);
-
-    // 打底灰色曲线
-    context.beginPath();
-    context.arc(0, 0, r, 0, 2*Math.PI);
-    context.setLineWidth(12);
-    context.setStrokeStyle('#f0f0f0');
-    context.stroke();
-
-    // 画渐变曲线
-    context.beginPath();
-    context.arc(0, 0, r, 0, deg);
-    context.setLineWidth(12);
-    // context.setLineCap('round');
-    context.setStrokeStyle(grd);
-    context.stroke();
-
-    if (needDot) {
-      drawBarCoordinateDot({percentage, context, dotStyle});
-    }
-
-    wx.drawCanvas({
-      canvasId: 'progress',
-      actions: context.getActions()
-    });
-  }else{
-    console.error('canvasId不能为空');
-  }
-};
+export default MpProgress;
