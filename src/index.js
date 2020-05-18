7156a28606e3becedbc2dccf3494a6c25bbe3f97
lucaszhu <lucaszhu@webank.com>
2020-05-18 14:16:26 +0800

    FIX: 修复bar的宽度设置问题

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@
  */
 class MpProgress{
   constructor(options){
-    const {canvasId, canvasSize = {width: 0, height: 0}, barStyle = {}, needDot = false, dotStyle = []} = options;
+    const {canvasId, canvasSize = {width: 0, height: 0}, percent = 100, barStyle = {}, needDot = false, dotStyle = []} = options;
     if (canvasId) {
       this._context = wx.createContext();
+      // 定义展示圆环的百分比，百分比不少于50%
+      this._percent = percent < 50 ? 50 : percent;
       this._options = {
         canvasId,
         needDot,
@@ -37,7 +39,8 @@ class MpProgress{
       const originY = Math.round(this._options.canvasSize.height/2);
       this._context.translate(this.convertLength(originX), this.convertLength(originY));
       // arc原点默认为3点钟方向，需要调整到12点
-      this._context.rotate(-90 * Math.PI / 180);
+      const rotateDeg = this._percent === 100 ? -90 : (((100 - this._percent) + (50 - this._percent)/2)/100).toFixed(2)*360;
+      this._context.rotate(rotateDeg * Math.PI / 180);
 
       const {barStyle} = this._options;
       if (barStyle.length > 0) {
@@ -68,16 +71,15 @@ class MpProgress{
             return;
           }
         }
-
+        console.log(_r)
         this._r = this.convertLength(_r);
-        console.log(this._r);
 
         for (let i = 0, len = barStyle.length; i < barStyle.length; i++) {
           ((i)=>{
             const bar = barStyle[i];
             this._context.beginPath();
             this._context.arc(0, 0, this._r, 0, i === len - 1 ? deg : 2*Math.PI);
-            this._context.setLineWidth(bar.width);
+            this._context.setLineWidth(this.convertLength(bar.width));
             this._context.setStrokeStyle(this.generateBarFillStyle(bar.fillStyle));
             const barLineCap = bar.lineCap;
             if (barLineCap) {
