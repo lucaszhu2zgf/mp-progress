a3b2343d3bc80b96ce2d6ba7c01607c27c2d9568
lucaszhu <lucaszhu@webank.com>
2020-08-18 10:21:06 +0800

    1.2.12

--- dist/progress.js
+++ dist/progress.js
@@ -1,4 +1,4 @@
-/*! mp-progress.js v1.2.11 https://www.npmjs.com/package/mp-progress */
+/*! mp-progress.js v1.2.12 https://www.npmjs.com/package/mp-progress */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
